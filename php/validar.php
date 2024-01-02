0912488218903805cab66d988b961c2aac5af1f3
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2024-01-02 23:15:42 +0100

    Updates

--- php/validar.php
+++ php/validar.php
@@ -10,33 +10,33 @@
 		extract($_POST); // Generación de las variables a partir de los datos recibidos del formulario.
 
 		if (!isset($correo) || strlen($correo) < 5 || strlen($correo) > 320) {
-			echo "El <strong>correo</strong> electrónico ha de tener entre 5 y 320 caracteres <br>";
+			echo "El <strong>correo</strong> electrónico ha de tener entre 5 y 320 caracteres<br>";
 			$error = true;
 		} elseif (filter_var($correo, FILTER_VALIDATE_EMAIL) === false) {
-			echo "No es un <strong>correo</strong> electrónico válido <br>";
+			echo "No es un <strong>correo</strong> electrónico válido<br>";
 			$error = true;
 		}
 
 		if (!isset($clave) || strlen($clave) < 4 || strlen($clave) > 20) {
-			echo "La <strong>clave</strong> ha de tener entre 4 y 20 caracteres <br>";
+			echo "La <strong>clave</strong> ha de tener entre 4 y 20 caracteres<br>";
 			$error = true;
 		} elseif (!isset($clave_repe) || $clave !== $clave_repe) {
-			echo "Las <strong>claves</strong> han de ser iguales <br>";
+			echo "Las <strong>claves</strong> han de ser iguales<br>";
 			$error = true;
 		}
 
 		if (!isset($nombre) || strlen($nombre) < 3 || strlen($nombre) > 80) {
-			echo "El <strong>nombre</strong> ha de tener entre 3 y 80 caracteres <br>";
+			echo "El <strong>nombre</strong> ha de tener entre 3 y 80 caracteres<br>";
 			$error = true;
 		}
 
 		if (!isset($calle) || strlen($calle) < 5 || strlen($calle) > 40) {
-			echo "La <strong>calle</strong> ha de tener entre 5 y 40 caracteres <br>";
+			echo "La <strong>calle</strong> ha de tener entre 5 y 40 caracteres<br>";
 			$error = true;
 		}
 
 		if (!isset($numero) || !ctype_digit($numero) || $numero <= 0) {
-			echo "El <strong>número</strong> no es válido <br>";
+			echo "El <strong>número</strong> no es válido<br>";
 			$error = true;
 		}
 
@@ -44,13 +44,13 @@
 			//El piso puede estar vacío
 			//Si no está vacío y su longitud no es correcta, hay error
 			if (strlen($piso) < 3 || strlen($piso) > 20) {
-				echo "El <strong>piso</strong> ha de tener entre 3 y 20 caracteres <br>";
+				echo "El <strong>piso</strong> ha de tener entre 3 y 20 caracteres<br>";
 				$error = true;
 			}
 		}
 
 		if (!isset($poblacion) || strlen($poblacion) < 3 || strlen($poblacion) > 40) {
-			echo "La <strong>población</strong> ha de tener entre 3 y 40 caracteres <br>";
+			echo "La <strong>población</strong> ha de tener entre 3 y 40 caracteres<br>";
 			$error = true;
 		}
 
@@ -65,7 +65,7 @@
 		}
 
 		if (!isset($codigo_postal) || !ctype_digit($codigo_postal) || strlen($codigo_postal) != 5) {
-			echo "El <strong>código postal</strong> ha de tener 5 dígitos <br>";
+			echo "El <strong>código postal</strong> ha de tener 5 dígitos<br>";
 			$error = true;
 		}
 
@@ -75,25 +75,25 @@
 		}
 
 		if (!isset($mes, $dia, $anho) || !ctype_digit($mes) || !ctype_digit($dia) || !ctype_digit($anho) || !checkdate($mes, $dia, $anho)) {
-			echo "La <strong>fecha</strong> de nacimiento no es correcta <br>";
+			echo "La <strong>fecha</strong> de nacimiento no es correcta<br>";
 			$error = true;
 		} 
 		
 		if (mktime(0, 0, 0, $mes, $dia, $anho) > time()) {
-			echo "La <strong>fecha</strong> de nacimiento debe ser anterior a la actual <br>";
+			echo "La <strong>fecha</strong> de nacimiento debe ser anterior a la actual<br>";
 			$error = true;
 		}
 
 		if (isset($web) && $web != null) {
 			//Solo hay error si no está vacía y no es válida
 			if (filter_var($web, FILTER_VALIDATE_URL) === false) {
-				echo "La <strong>dirección web</strong> no es válida <br>";
+				echo "La <strong>dirección web</strong> no es válida<br>";
 				$error = true;
 			}
 		}
 
 		if (!isset($temas) || !is_array($temas) || count($temas) == 0) {
-			echo "No ha marcado ningún <strong>tema</strong> <br>";
+			echo "No ha marcado ningún <strong>tema</strong><br>";
 			$error = true;
 		}
 
@@ -102,7 +102,7 @@
 				"Debe corregir los errores. <a href='javascript:history.back();'>Volver</a>"
 			);
 		} else {
-			echo "<strong>Procesando registro...</strong> <br />";
+			echo "<strong>Procesando registro...</strong><br>";
 			/* INSERCIÓN EN LA BASE DE DATOS */
 
 			$servidor = "localhost";
