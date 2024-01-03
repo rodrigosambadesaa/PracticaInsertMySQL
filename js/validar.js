eb2d73411bca2582662d1ab498674acbfdd9356a
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2024-01-03 19:51:31 +0100

    Updates

--- js/validar.js
+++ js/validar.js
@@ -1,10 +1,18 @@
 // Obtener el formulario por su ID
 const formulario = document.getElementById('formulario');
 
+// Crear un div para los mensajes de error
+const errorDiv = document.createElement('div');
+errorDiv.classList.add('error-message');
+formulario.appendChild(errorDiv);
+
 // Agregar un evento de escucha para el envío del formulario
 formulario.addEventListener('submit', function (event) {
     // Detener el envío del formulario
     event.preventDefault();
+
+    // Limpiar los mensajes de error
+    errorDiv.innerHTML = '';
 
     // Realizar las validaciones necesarias
     const nombre = document.getElementById('nombre').value;
@@ -23,161 +31,170 @@ formulario.addEventListener('submit', function (event) {
     const fechaNacimientoAnio = document.getElementById('fechaNacimientoAnio').value;
     const paginaWeb = document.getElementById('paginaWeb').value;
 
+    let errorMessages = "";
+    let error = false;
+
     // Validar el email
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email) || email.length < 3 || email.length > 320) {
-        alert('Por favor, introduce un email válido entre 3 y 320 caracteres');
-        return;
+        errorMessages += 'Por favor, introduce un email válido entre 3 y 320 caracteres<br>';
+        error = true;
     }
 
     // Validar la contraseña
     if (password.length < 4 || password.length > 20) {
-        alert('La contraseña debe tener entre 4 y 20 caracteres');
-        return;
+        errorMessages+= 'La contraseña debe tener entre 4 y 20 caracteres'
+        error = true;
     }
 
     // Validar que las contraseñas coincidan
     if (password !== confirmPassword) {
-        alert('Las contraseñas no coinciden');
-        return;
+        errorMessages += 'Las contraseñas no coinciden<br>';
+        error = true;
     }
 
     // Validar que se haya ingresado un nombre y apellidos
     if (nombre.trim() === '') {
-        alert('Por favor, introduce tu nombre y apellidos');
-        return;
+        errorMessages += 'Por favor, introduce tu nombre y apellidos<br>';
+        error = true;
     }
 
     // Validar el nombre
     if (nombre.length < 3 || nombre.length > 80) {
-        alert('Por favor, introduce un nombre válido entre 3 y 80 caracteres');
-        return;
+        errorMessages += 'Por favor, introduce un nombre válido entre 3 y 80 caracteres<br>';
+        error = true;
     }
 
     // Validar que se haya ingresado una dirección de calle
     if (direccionCalle.trim() === '') {
-        alert('Por favor, introduce tu dirección de calle');
-        return;
+       errorMessages += 'Por favor, introduce tu dirección de calle<br>';
+       error = true;
     }
 
     // Validar la longitud de la dirección de calle
     if (direccionCalle.length < 5 || direccionCalle.length > 40) {
-        alert('Por favor, introduce una calle válida entre 5 y 40 caracteres');
-        return;
+       errorMessages += 'Por favor, introduce una dirección de calle válida entre 5 y 40 caracteres<br>';
+       error = true;
     }
 
     // Validar que se haya ingresado un número de dirección
     if (direccionNumero.trim() === '') {
-        alert('Por favor, introduce el número de tu dirección');
-        return;
+       errorMessages += 'Por favor, introduce tu número de dirección<br>';
+       error = true;
     }
 
     // Validar que el número de dirección sea un número natural positivo
     if (isNaN(direccionNumero) || direccionNumero <= 0 || direccionNumero % 1 !== 0) {
-        alert('Por favor, introduce un número de dirección válido');
-        return;
+        errorMessages += 'Por favor, introduce un número de dirección válido<br>';
+        error = true;
     }
 
     // Validar el piso
     if (direccionPiso.trim() !== '' && (direccionPiso.length < 3 || direccionPiso.length > 20)) {
-        alert('Por favor, introduce un piso válido entre 3 y 20 caracteres');
-        return;
+        errorMessages += 'Por favor, introduce un piso válido entre 3 y 20 caracteres<br>';
+        error = true;
     }
 
     // Validar que se haya ingresado una población
     if (poblacion.trim() === '') {
-        alert('Por favor, introduce tu población');
-        return;
+       errorMessages += 'Por favor, introduce tu población<br>';
+       error = true;
     }
     // Validar que se haya ingresado una población válida
     if (poblacion.length < 3 || poblacion.length > 40) {
-        alert('Por favor, introduce una población válida entre 3 y 40 caracteres');
-        return;
+       errorMessages += 'Por favor, introduce una población válida entre 3 y 40 caracteres<br>';
+       error = true;
     }
 
     // Validar que se haya ingresado una provincia
     if (provincia.trim() === '') {
-        alert('Por favor, introduce tu provincia');
-        return;
+        errorMessages += 'Por favor, selecciona una provincia<br>';
+        error = true;
     }
 
     // Validar que el valor de la provincia esté entre 1 y 4
     if (provincia < 1 || provincia > 4) {
-        alert('Por favor, selecciona una provincia válida');
-        return;
+        errorMessages += 'Por favor, selecciona una provincia válida<br>';
+        error = true;
     }
 
     // Validar que la provincia sea un entero
     if (isNaN(provincia) || !Number.isInteger(Number(provincia))) {
-        alert('Por favor, selecciona una provincia válida, numérica en forma de enteros');
-        return;
+       errorMessages += 'Por favor, selecciona una provincia válida<br>';
+       error = true;
     }
 
     // Validar que se haya ingresado un código postal
     if (codigoPostal.trim() === '') {
-        alert('Por favor, introduce tu código postal');
-        return;
+        errorMessages += 'Por favor, introduce tu código postal<br>';
+        error = true;
     }
 
     // Validar que el código postal sea un entero de 5 dígitos
     if (isNaN(codigoPostal) || !Number.isInteger(Number(codigoPostal)) || codigoPostal.length !== 5) {
-        alert('Por favor, introduce un código postal válido de 5 dígitos');
-        return;
+        errorMessages += 'Por favor, introduce un código postal válido de 5 dígitos<br>';
+        error = true;
     }
 
     // Validar que se haya seleccionado un estado civil
     if (estadoCivil === '' || estadoCivil.length !== 1) {
-        alert('Por favor, selecciona tu estado civil');
-        return;
+        errorMessages += 'Por favor, selecciona un estado civil<br>';
+        error = true;
     }
 
     // Validar que se haya ingresado una fecha de nacimiento
     if (fechaNacimientoDia === '' || fechaNacimientoMes === '' || fechaNacimientoAnio === '') {
-        alert('Por favor, introduce tu fecha de nacimiento completa');
-        return;
+        errorMessages += 'Por favor, introduce tu fecha de nacimiento<br>';
+        error = true;
     }
 
     // Validar que el día de la fecha de nacimiento sea un entero positivo de dos dígitos como máximo
     if (isNaN(fechaNacimientoDia) || !Number.isInteger(Number(fechaNacimientoDia)) || fechaNacimientoDia <= 0 || fechaNacimientoDia > 31) {
-        alert('Por favor, introduce un día de nacimiento válido');
-        return;
+        errorMessages += 'Por favor, introduce un día de nacimiento válido<br>';
+        error = true;
     }
 
     // Validar que el mes de la fecha de nacimiento sea un entero positivo de dos dígitos como máximo
     if (isNaN(fechaNacimientoMes) || !Number.isInteger(Number(fechaNacimientoMes)) || fechaNacimientoMes <= 0 || fechaNacimientoMes > 12) {
-        alert('Por favor, introduce un mes de nacimiento válido');
-        return;
+        errorMessages += 'Por favor, introduce un mes de nacimiento válido<br>';
+        error = true;
     }
 
     // Validar que el año de la fecha de nacimiento sea un entero positivo de dos dígitos como máximo
     if (isNaN(fechaNacimientoAnio) || !Number.isInteger(Number(fechaNacimientoAnio))) {
-        alert('Por favor, introduce un año de nacimiento válido');
-        return;
+        errorMessages += 'Por favor, introduce un año de nacimiento válido<br>';
+        error = true;
     }
 
     // Validar que la fecha de nacimiento sea anterior a la fecha actual
     const fechaNacimiento = new Date(fechaNacimientoAnio, fechaNacimientoMes - 1, fechaNacimientoDia);
     const fechaActual = new Date();
     if (fechaNacimiento >= fechaActual) {
-        alert('Por favor, introduce una fecha de nacimiento anterior a la fecha actual');
-        return;
+        errorMessages += 'Por favor, introduce una fecha de nacimiento válida<br>';
+        error = true;
     }
 
     if (paginaWeb.trim() !== '') {
         try {
             const paginaWebUrl = new URL(paginaWeb);
         } catch (error) {
-            alert('Por favor, introduce una URL válida');
-            return;
+            errorMessages += 'Por favor, introduce una página web válida<br>';
+            error = true;
         }
     }
 
     // Validar que al menos un tema esté seleccionado
     const temasSeleccionados = document.querySelectorAll('input[name="temas[]"]:checked');
     if (temasSeleccionados.length === 0) {
-        alert('Por favor, selecciona al menos un tema');
-        return;
+        errorMessages += 'Por favor, selecciona al menos un tema<br>';
+        error = true;
     }
 
-    formulario.submit();
+    if (error) {
+        errorDiv.innerHTML = errorMessages;
+        return;
+    } else {
+        formulario.submit();
+    }
+
 });
